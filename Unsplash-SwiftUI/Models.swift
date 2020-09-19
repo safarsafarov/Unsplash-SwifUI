98a1067bfee094512aa605a990ba387d6e476c9c
safarsafarov <safarov.safar@hotmail.com>
2020-09-20 03:56:11 +0800

    first

---
+++ Unsplash-SwiftUI/Models.swift
@@ -0,0 +1,25 @@
+//
+//  Models.swift
+//  Unsplash-SwiftUI
+//
+//  Created by Safar Safarov on 2020/9/20.
+//
+
+import SwiftUI
+
+struct Model: Identifiable {
+    let id: String
+    let name: String
+    let bio: String
+    var image: UIImage
+    
+    init(user: User) {
+        self.id = user.id
+        self.name = user.name
+        self.bio = user.bio ?? "No Bio"
+        if let url = URL(string: user.profileImage.medium) {
+            let data = try! Data(contentsOf: url)
+            self.image = UIImage(data: data)
+        }
+    }
+}
