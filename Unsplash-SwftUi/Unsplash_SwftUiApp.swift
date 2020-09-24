f2852eb18ca95dd228271d6e327668cd8bfb73bd
safarsafarov <safarov.safar@hotmail.com>
2020-09-25 01:17:50 +0800

    1e

---
+++ Unsplash-SwftUi/Unsplash_SwftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unsplash_SwftUiApp.swift
+//  Unsplash-SwftUi
+//
+//  Created by Safar Safarov on 2020/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Unsplash_SwftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
